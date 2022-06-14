c6b2bf184692803739acfbd1e9e1019164583b4e
Calvin Friedrich <calvin.friedrich@ibm.com>
2022-06-14 17:38:40 +0200

    Basic Input Validation

--- GymMaster/GymMaster/Views/Studios/StudioDetailView.swift
+++ GymMaster/GymMaster/Views/Studios/StudioDetailView.swift
@@ -27,90 +27,119 @@ struct StudioDetailView: View {
     @State var personalKosten: Double = 0.0
     @State var mitgliederEinnahmen: Double = 0.0
     
+    func CheckIfDouble (_ str: String) -> Bool {
+        guard Double(str) != nil else {return false}
+        return true
+    }
+    
+    
     var body: some View {
-            Form {
-                Section(header: Text("Generelle Daten")) {
-                    ZStack(alignment: .leading) {
-                        Text("Name")
-                            .foregroundColor(name.isEmpty ? Color(.placeholderText) : .accentColor)
-                            .offset(y: name.isEmpty ? 0 : -25)
-                            .scaleEffect(name.isEmpty ? 1 : 0.8, anchor: .leading)
-                        TextField("Name", text: $name)
-                    }
-                    .padding(.top)
-                    .animation(.default)
-                    
-                    ZStack(alignment: .leading) {
-                        Text("Mitgliedschaft in € / Monat")
-                            .foregroundColor(mitgliedschaft.isEmpty ? Color(.placeholderText) : .accentColor)
-                            .offset(y: mitgliedschaft.isEmpty ? 0 : -25)
-                            .scaleEffect(mitgliedschaft.isEmpty ? 1 : 0.8, anchor: .leading)
-                        TextField("Mitgliedschaft in € / Monat", text: $mitgliedschaft)
-                            .onChange(of: mitgliedschaft, perform: { _ in
+        Form {
+            Section(header: Text("Generelle Daten")) {
+                ZStack(alignment: .leading) {
+                    Text("Name")
+                        .foregroundColor(name.isEmpty ? Color(.placeholderText) : .accentColor)
+                        .offset(y: name.isEmpty ? 0 : -25)
+                        .scaleEffect(name.isEmpty ? 1 : 0.8, anchor: .leading)
+                    TextField("Name", text: $name)
+                }
+                .padding(.top)
+                .animation(.default)
+                
+                ZStack(alignment: .leading) {
+                    Text("Mitgliedschaft in € / Monat")
+                        .foregroundColor(mitgliedschaft.isEmpty ? Color(.placeholderText) : .accentColor)
+                        .offset(y: mitgliedschaft.isEmpty ? 0 : -25)
+                        .scaleEffect(mitgliedschaft.isEmpty ? 1 : 0.8, anchor: .leading)
+                    TextField("Mitgliedschaft in € / Monat", text: $mitgliedschaft)
+                        .onChange(of: mitgliedschaft){
+                            newValue in if !CheckIfDouble(mitgliedschaft) {
+                                mitgliedschaft = String(newValue.dropLast())
+                            } else {
                                 mitgliederEinnahmen = Double(round(100 * Double(mitglieder.count) * (Double(mitgliedschaft) ?? 0)) / 100)
-                            })
-                    }
-                    .padding(.top)
-                    .animation(.default)
-                    
-                    ZStack(alignment: .leading) {
-                        Text("Latitude")
-                            .foregroundColor(latitude.isEmpty ? Color(.placeholderText) : .accentColor)
-                            .offset(y: latitude.isEmpty ? 0 : -25)
-                            .scaleEffect(latitude.isEmpty ? 1 : 0.8, anchor: .leading)
-                        TextField("Latitude", text: $latitude)
-                    }
-                    .padding(.top)
-                    .animation(.default)
-                    
-                    ZStack(alignment: .leading) {
-                        Text("Longitude")
-                            .foregroundColor(longitude.isEmpty ? Color(.placeholderText) : .accentColor)
-                            .offset(y: longitude.isEmpty ? 0 : -25)
-                            .scaleEffect(longitude.isEmpty ? 1 : 0.8, anchor: .leading)
-                        TextField("Longitude", text: $longitude)
-                    }
-                    .padding(.top)
-                    .animation(.default)
-                    
-                    ZStack(alignment: .leading) {
-                        Text("Maintenance in € / Monat")
-                            .foregroundColor(maintenance.isEmpty ? Color(.placeholderText) : .accentColor)
-                            .offset(y: maintenance.isEmpty ? 0 : -25)
-                            .scaleEffect(maintenance.isEmpty ? 1 : 0.8, anchor: .leading)
-                        TextField("Maintenance in € / Monat", text: $maintenance)
-                    }
-                    .padding(.top)
-                    .animation(.default)
-                    
-                    Button("Speichern") {
-                        let index =  studioStorage.allStudios.firstIndex(where: {$0.id == studioID})
-                        studioStorage.allStudios[index!].name = name
-                        studioStorage.allStudios[index!].mitgliedschaft = Double(mitgliedschaft) ?? 0
-                        studioStorage.allStudios[index!].latitude = Double(latitude) ?? 0
-                        studioStorage.allStudios[index!].longitude = Double(longitude) ?? 0
-                        studioStorage.allStudios[index!].maintenance = Double(maintenance) ?? 0
-                        studioStorage.saveChanges()
-                        showingAlert = true
-                    }
+                            }
+                        }
+                        .keyboardType(.numberPad)
                 }
+                .padding(.top)
+                .animation(.default)
                 
-                Section(header: Text("Information")) {
-                    Text("Personalkosten: \(String(format: "%.2f", personalKosten)) € / Monat")
-                    Text("Gesamtkosten: \(String(format: "%.2f", personalKosten + (Double(maintenance) ?? 0))) € / Monat")
-                    Text("Mitgliedschaftseinnahmen: \(String(format: "%.2f", mitgliederEinnahmen)) € / Monat")
-                    Text("Profit: \(String(format: "%.2f", mitgliederEinnahmen - (personalKosten + (Double(maintenance) ?? 0)))) € / Monat")
+                ZStack(alignment: .leading) {
+                    Text("Latitude")
+                        .foregroundColor(latitude.isEmpty ? Color(.placeholderText) : .accentColor)
+                        .offset(y: latitude.isEmpty ? 0 : -25)
+                        .scaleEffect(latitude.isEmpty ? 1 : 0.8, anchor: .leading)
+                    TextField("Latitude", text: $latitude)
+                        .onChange(of: latitude){
+                            newValue in if !CheckIfDouble(latitude) {
+                                latitude = String(newValue.dropLast())
+                            }
+                        }
+                        .keyboardType(.numberPad)
                 }
+                .padding(.top)
+                .animation(.default)
                 
-                Section(header: Text("Verwaltung")) {
-                    NavigationLink(destination: MitgliederView()){
-                        Text("Mitglieder")
-                    }
-                    NavigationLink(destination: PersonalView()){
-                        Text("Personal")
-                    }
+                ZStack(alignment: .leading) {
+                    Text("Longitude")
+                        .foregroundColor(longitude.isEmpty ? Color(.placeholderText) : .accentColor)
+                        .offset(y: longitude.isEmpty ? 0 : -25)
+                        .scaleEffect(longitude.isEmpty ? 1 : 0.8, anchor: .leading)
+                    TextField("Longitude", text: $longitude)
+                        .onChange(of: longitude){
+                            newValue in if !CheckIfDouble(longitude) {
+                                longitude = String(newValue.dropLast())
+                            }
+                        }
+                        .keyboardType(.numberPad)
+                }
+                .padding(.top)
+                .animation(.default)
+                
+                ZStack(alignment: .leading) {
+                    Text("Maintenance in € / Monat")
+                        .foregroundColor(maintenance.isEmpty ? Color(.placeholderText) : .accentColor)
+                        .offset(y: maintenance.isEmpty ? 0 : -25)
+                        .scaleEffect(maintenance.isEmpty ? 1 : 0.8, anchor: .leading)
+                    TextField("Maintenance in € / Monat", text: $maintenance)
+                        .onChange(of: maintenance){
+                            newValue in if !CheckIfDouble(maintenance) {
+                                maintenance = String(newValue.dropLast())
+                            }
+                        }
+                        .keyboardType(.numberPad)
+                }
+                .padding(.top)
+                .animation(.default)
+                
+                Button("Speichern") {
+                    let index =  studioStorage.allStudios.firstIndex(where: {$0.id == studioID})
+                    studioStorage.allStudios[index!].name = name
+                    studioStorage.allStudios[index!].mitgliedschaft = Double(mitgliedschaft) ?? 0
+                    studioStorage.allStudios[index!].latitude = Double(latitude) ?? 0
+                    studioStorage.allStudios[index!].longitude = Double(longitude) ?? 0
+                    studioStorage.allStudios[index!].maintenance = Double(maintenance) ?? 0
+                    studioStorage.saveChanges()
+                    showingAlert = true
                 }
             }
+            
+            Section(header: Text("Information")) {
+                Text("Personalkosten: \(String(format: "%.2f", personalKosten)) € / Monat")
+                Text("Gesamtkosten: \(String(format: "%.2f", personalKosten + (Double(maintenance) ?? 0))) € / Monat")
+                Text("Mitgliedschaftseinnahmen: \(String(format: "%.2f", mitgliederEinnahmen)) € / Monat")
+                Text("Profit: \(String(format: "%.2f", mitgliederEinnahmen - (personalKosten + (Double(maintenance) ?? 0)))) € / Monat")
+            }
+            
+            Section(header: Text("Verwaltung")) {
+                NavigationLink(destination: MitgliederView()){
+                    Text("Mitglieder")
+                }
+                NavigationLink(destination: PersonalView()){
+                    Text("Personal")
+                }
+            }
+        }
         .lazyPop()
         .listStyle(GroupedListStyle())
         .onAppear(perform: {
